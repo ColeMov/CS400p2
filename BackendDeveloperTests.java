a9d201e1ae53758c87436cd32d6a7fe7f5886abb
Cole Movsessian <movsessian@wisc.edu>
2023-11-20 10:48:24 -0600

    Added more classes

---
+++ BackendDeveloperTests.java
@@ -0,0 +1,51 @@
+// --== CS400 Fall 2023 File Header Information ==--
+// Name: Cole Movsessian
+// Email: movsessian@wisc.edu
+// Group: G37
+// TA: Grant Waldow
+// Lecturer: Florian Heimerl
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit tests for the Backend Developer's implementation of the Backend interface.
+ * These tests verify the functionality of methods related to reading and managing meteorite data.
+ */
+
+public class BackendDeveloperTests {
+    private BackendImplementation backend;
+
+    /**
+     * Tests that the readData method returns true upon success
+     */
+    @Test
+    public void readFileDataTest(){
+
+    }
+
+    /**
+     * Tests that the readData method returns NoFileFoundException upon failure
+     */
+    @Test
+    public void readFileDataNoFileFoundTest(){
+
+    }
+
+    /**
+     * Tests getShortestPath method returns a ShortestPath object
+     */
+    @Test
+    public void shortestPathTest(){
+
+    }
+
+    /**
+     * Tests statistics method returns double list with shortest path statistics
+     */
+    @Test
+    public void statisticsTest(){
+
+    }
+}
---
+++ BackendImplementation.java
@@ -0,0 +1,52 @@
+// --== CS400 Fall 2023 File Header Information ==--
+// Name: Cole Movsessian
+// Email: movsessian@wisc.edu
+// Group: G37
+// TA: Grant Waldow
+// Lecturer: Florian Heimerl
+
+import java.io.FileNotFoundException;
+
+/**
+ * This interface contains the methods used in the backend of the UW Path Finder app
+ */
+public class BackendInterface {
+  /*
+  CONSTRUCTOR:
+  public Backend(GraphADT graph)
+   */
+
+    /**
+     * Reads the data from the provided file path and parses it into this Backend's graph
+     * @param filepath The path to the file to read graph data from
+     * @throws FileNotFoundException If the data is unable to be properly read
+     * @return True if file is successfully loaded
+     */
+    public boolean readData(String filepath) throws FileNotFoundException{
+
+    }
+
+    /**
+     * Returns a ShortestPath object that encapsulates the shortest path between the provided
+     * origin and destination buildings
+     * @param origin The building that the shortest path starts at
+     * @param destination The building that the shortest path ends at
+     * @return An object containing the shortest path
+     */
+    public ShortestPathInterface getShortestPath(String origin, String destination){
+
+    }
+
+
+    /**
+     * Returns a list of statistics about the provided path:
+     * <li> Number of nodes (buildings)
+     * <li> Number of edges
+     * <li> Total walking time
+     * @param path The shortest path between two nodes
+     * @return An array containing the information about the path
+     */
+    public Double[] getStatistics(ShortestPathInterface path){
+
+    }
+}
