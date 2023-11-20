00805bafa5aa6564321c4743252e6303b410e1d2
Cole Movsessian <movsessian@wisc.edu>
2023-11-20 10:18:31 -0600

    Added interfaces

---
+++ ShortestPathInterface.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+/**
+ * This interface contains the methods to store the results of the shortest path search
+ */
+public interface ShortestPathInterface
+{
+  /*
+  Fields:
+  private String origin;
+  private String destination;
+
+  CONSTRUCTOR:
+  protected ShortestPathInterface(String origin, String destination)
+  {
+    this.origin = origin;
+    this.destination = destination;
+  }
+   */
+
+  /**
+   * Returns a list of buildings along the shortest path between the origin and destination
+   * buildings
+   * @return The list of buildings along the shortest path
+   */
+  List<String> getPathSegments();
+
+  /**
+   * Returns the walking time along each segment of the shortest path between the origin and
+   * destination buildings
+   * @return The list of walking times along the shortest path
+   */
+  List<Double> getWalkingTime();
+
+  /**
+   * Returns the total walking time along the shortest path between the origin and destination
+   * buildings
+   * @return The total walking time along the shortest path, in seconds
+   */
+  double getTotalPathCost();
+}
